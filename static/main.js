e12561eb580f09cd85440256020eb389f22686f6
CloudOps <123254093+Ethan41798@users.noreply.github.com>
2024-08-10 17:00:21 +0530

    Add files via upload

---
+++ static/main.js
@@ -0,0 +1,10 @@
+/** slider **/
+$(".slider-one")
+.not(".slick-intialized")
+.slick({
+    autoplay: true,
+    autospeed: 30,
+    dots: true,
+    prevArrow: ".site-slider .slider-btn .prev",
+    nextArrow: ".site-slider .slider-btn .next",
+});
